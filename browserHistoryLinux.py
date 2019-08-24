d7c17e8416b0113823cdc58686acd0168ff7583d
Viresh Uberoy <48689761+TheAwesomezter@users.noreply.github.com>
2019-08-24 09:12:06 +0530

    Update browserHistoryLinux.py

--- browserHistoryLinux.py
+++ browserHistoryLinux.py
@@ -9,14 +9,10 @@ c = con.cursor()
 c.execute('select * from urls');
 results = c.fetchall()
 
-# names = [description[0] for description in c.description]
-# with open('browserHistoryLinux.csv', 'w+', encoding='utf-8') as file:
-#     writer = csv.writer(file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-#     writer.writerow(names)
-#     for result in results:
-#         writer.writerow(result)
-#
-# print("File Written")
-
-for result in results:
-    print(result)
+names = [description[0] for description in c.description]
+with open('browserHistoryLinux.csv', 'w+', encoding='utf-8') as file:
+    writer = csv.writer(file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+    writer.writerow(names)
+    for result in results:
+        writer.writerow(result)
+        print("File Written")
